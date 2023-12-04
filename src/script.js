e0f1d5e13bbeff6732653f7ba301fa1b68d4f1ea
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2023-12-04 17:17:37 +0100

    Added the AI integration and included axios plugin

--- src/script.js
+++ src/script.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function showPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Herinnering aan Holland",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 1,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let informationInput = document.querySelector("#user-information");
+  let apiKey = "b4fb306o7t4d0bd72a6a1646e9352dd7";
+  let context =
+    "You are a knowledgeable and soulful expert who loves to write short and unique poems. Your goal is to generate a 5 line poem in basic HTML separating each line with a <br/>. Please ensure that you follow the user information and it is not necessary to  include the title of the poem. Make sure to sign at the end of the poem with  'Jessica Bot' inside a <strong><em> element";
+  let prompt = `User informations:Generate a Dutch poem about ${informationInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(showPoem);
 }
 
 let poemElement = document.querySelector("#poem-generator");
