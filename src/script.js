e14d828fd97e39991519cae4f7fe435cdbe11f6e
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2023-12-04 18:15:08 +0100

    Added Ephesis font family and updated CSS

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let informationInput = document.querySelector("#user-information");
   let apiKey = "b4fb306o7t4d0bd72a6a1646e9352dd7";
   let context =
-    "You are a knowledgeable and soulful expert who loves to write short and unique poems. Your goal is to generate a 5 line poem in basic HTML separating each line with a <br/>. Please ensure that you follow the user information and it is not necessary to  include the title of the poem. Make sure to sign at the end of the poem with  'Jessica Bot' inside a <strong><em> element";
+    "You are a knowledgeable and soulful expert who loves to write short and unique poems. Your goal is to generate a 5 line poem in basic HTML separating each line with a <br/>. Please ensure that you follow the user information and it is not necessary to  include the title of the poem. Make sure to sign at the end of the poem with  'Jessica Bot' inside a <strong> element.Do NOT include the sign in the beginning";
   let prompt = `User informations:Generate a Dutch poem about ${informationInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Generating poem");
