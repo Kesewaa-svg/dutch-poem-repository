bf5e37eeac25c39a175a0bf5af1286d5b99a4aa7
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2023-12-05 18:56:10 +0100

    Improved User Experience

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ function showPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: 1,
+    delay: 5,
   });
 }
 
@@ -13,9 +13,14 @@ function generatePoem(event) {
   let informationInput = document.querySelector("#user-information");
   let apiKey = "b4fb306o7t4d0bd72a6a1646e9352dd7";
   let context =
-    "You are a knowledgeable and soulful expert who loves to write short and unique poems. Your goal is to generate a 5 line poem in basic HTML separating each line with a <br/>. Please ensure that you follow the user information and it is not necessary to  include the title of the poem. Make sure to sign at the end of the poem with  'Jessica Bot' inside a <strong> element.Do NOT include the sign in the beginning";
+    "You are a knowledgeable and soulful expert who loves to write short and unique poems. Your goal is to generate a 5 line poem in basic HTML separating each line with a <br/>. Please ensure that you follow the user information and it is not necessary to  include the title of the poem. Make sure to sign at the end of the poem with  'Kess Bot'  .Do NOT include the sign in the beginning";
   let prompt = `User informations:Generate a Dutch poem about ${informationInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemDisplay = document.querySelector("#poem");
+  poemDisplay.classList.remove("hidden");
+  poemDisplay.innerHTML = `<div class="generating"> ⏳ Generating a Dutch poem about ${informationInput.value} </div>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
